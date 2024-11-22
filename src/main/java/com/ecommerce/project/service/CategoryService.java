3b31bf5e57313b83873a96a3434b12c70d79f0df
Sr1jan27 <singhsrijan809@gmail.com>
2024-11-22 10:03:15 +0530

    api to delete category and also validate if the category is there or not then only delete then category

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface CategoryService {
     List<Category> getAllCategories();
     void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
 }
