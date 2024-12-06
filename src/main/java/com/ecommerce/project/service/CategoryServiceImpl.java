18aff93215e506c1f9ef6553937a580f1f03bc0d
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-06 22:24:35 +0530

    adding exception to tell no categories addded

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -5,14 +5,9 @@ import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -25,6 +20,10 @@ public class CategoryServiceImpl implements CategoryService {
 //    List<Category> categories = categoryRepository.findAll();
     @Override
     public List<Category> getAllCategories() {
+        List<Category> categoryList = categoryRepository.findAll();
+        if(categoryList.isEmpty()){
+            throw new APIException("No category present in database");
+        }
         return categoryRepository.findAll();
     }
 
