7e93b3e37d54446987cda08ba9e65f61bda7c035
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-06 22:16:24 +0530

    making custom exception and using it with the service

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.APIException;
 import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
@@ -29,8 +30,11 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+        if(savedCategory != null){
+            throw new APIException("Category with the name already present");
+        }
         category.setCategoryId(nextId++);
-//        categories.add(category);
         categoryRepository.save(category);
     }
 
