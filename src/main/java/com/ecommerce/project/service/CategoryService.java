59a8dbecad20484007bf061d31a2b4f2072ee27a
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-08 19:03:30 +0530

    accepting pagination

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 public interface CategoryService {
-    CategoryResponse getAllCategories();
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
     CategoryDTO createCategory(CategoryDTO categroyDTO);
 
     CategoryDTO deleteCategory(Long categoryId);
