d08f589b46b4a54d87d1c144dd9cac563d0a540c
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-09 07:57:11 +0530

    refacotring

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,11 +1,8 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 
 public interface CategoryService {
