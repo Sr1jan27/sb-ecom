e132a035b00511c5b4baf44d575b16eb79812959
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-11 08:57:23 +0530

    add product

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -1,7 +1,13 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService{
+    @Override
+    public ProductDTO addProduct(Product product, Long categoryId) {
+        return null;
+    }
 }
