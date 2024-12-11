6a6b101e01194dae111e2ce5a4b773f8a8eb916e
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-11 08:37:49 +0530

    defined all property

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -1,5 +1,20 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Product {
+
+    private Long productId;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
