6d52a17f7da47898a257bed59088c7442bd5f48f
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-12 17:44:24 +0530

    adding image

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -15,6 +15,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
     private String productName;
+    private String image;
     private String description;
     private Integer quantity;
     private double price;
