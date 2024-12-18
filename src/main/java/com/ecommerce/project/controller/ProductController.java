5319dfbc525b81babd4a03b898346fdfd809218b
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-18 22:31:24 +0530

    api endpoint changes

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -40,7 +40,7 @@ public class ProductController {
         return new ResponseEntity<>(productResponse, HttpStatus.FOUND);
     }
 
-    @PutMapping("/products/{productId}")
+    @PutMapping("/admin/products/{productId}")
     public ResponseEntity<ProductDTO> updateProduct(@RequestBody Product product, @PathVariable Long productId){
         ProductDTO productDTO = productService.updateProduct(product, productId);
         return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
