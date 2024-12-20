a10b2685f6adfffe33d1fcdb329e01f4e0db96db
Sr1jan27 <singhsrijan809@gmail.com>
2024-12-20 10:32:57 +0530

    isssue understandings

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -124,8 +124,8 @@ public class ProductServiceImpl implements ProductService{
     private String uploadImage(String path, MultipartFile file) throws IOException {
         String originalFileName = file.getOriginalFilename(); // this will return  whole file name with the extension also
         String randomId = UUID.randomUUID().toString();
-        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf(".")));
-        String filePath = path + File.pathSeparator +fileName;
+        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
+        String filePath = path + File.separator +fileName;
 
         File folder = new File(path);
         if(!folder.exists()){
