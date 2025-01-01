a9b0d50a97b0660576aa2e651fdf4f1e3b598716
Sr1jan27 <singhsrijan809@gmail.com>
2025-01-01 15:46:34 +0530

    creation of model classes and enum for roles

---
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,34 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userName;
+    @NotBlank
+    @Size(max = 50)
+    private String email;
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+}
